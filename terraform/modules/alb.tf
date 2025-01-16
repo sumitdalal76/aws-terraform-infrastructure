844f399e8661a7e443fe7f47082c591aebba8dfd
sumit dalal <sumitdalal76@gmail.com>
2025-01-15 22:36:54 -0500

    AWS-terraform-infrastructure

--- terraform/modules/alb.tf
+++ terraform/modules/alb.tf
@@ -28,21 +28,3 @@ resource "aws_lb_listener" "http" {
     }
   }
 }
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.certificate_arn
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Service is healthy"
-      status_code  = "200"
-    }
-  }
-} 
