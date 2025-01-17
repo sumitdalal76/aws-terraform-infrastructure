e99c899a6650ddf108f4e44021bca0abf3b04cd5
sumit dalal <sumitdalal76@gmail.com>
2025-01-17 12:16:50 -0500

    aws-terraform-infra

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -1,4 +1,4 @@
-output "security_group_id" {
-  description = "ID of the web security group"
+output "alb_sg_id" {
+  description = "Security Group ID for the Application Load Balancer and web instances"
   value       = aws_security_group.web.id
 }
