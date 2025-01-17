eb4dd3469fd1c0b90ede6ffab04cf2c3ec5b30f1
sumit dalal <sumitdalal76@gmail.com>
2025-01-17 11:31:23 -0500

    aws-terraform-infra

--- modules/EC2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.web.id
-}
-
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.web.public_ip
-}
