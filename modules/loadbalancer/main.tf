342b7dc8af8d8231108c2e3849ad37dba6c6eb45
sumit dalal <sumitdalal76@gmail.com>
2025-01-16 20:56:31 -0500

    aws-terraform-infra

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -16,30 +16,25 @@ resource "aws_lb_listener" "http" {
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
   }
 }
 
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+#resource "aws_lb_listener" "https" {
+#  load_balancer_arn = aws_lb.main.arn
+#  port              = "443"
+#  protocol          = "HTTPS"
+#  ssl_policy        = "ELBSecurityPolicy-2016-08"
 #  certificate_arn   = var.certificate_arn
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Hello, World!"
-      status_code  = "200"
-    }
-  }
-}
+#
+#  default_action {
+#    type = "fixed-response"
+#
+#    fixed_response {
+#      content_type = "text/plain"
+#      message_body = "Hello, World!"
+#      status_code  = "200"
+#    }
+#  }
+#}
